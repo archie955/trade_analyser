0b0d90367a49182cbfc32f89bacba4e63158a6d2
archie955 <archiesmedley@gmail.com>
2026-04-14 23:29:52 +0100

    create league injection function

--- server/authentication/auth.py
+++ server/authentication/auth.py
@@ -1,7 +1,7 @@
 import jwt
 from fastapi.security import OAuth2PasswordBearer
 from utils.config import settings
-from fastapi import HTTPException, status, Depends
+from fastapi import HTTPException, status, Depends, Path
 from datetime import datetime, timezone, timedelta
 from typing import Dict, Any
 from models import schemas, models
@@ -73,7 +73,18 @@ def get_current_user(
     return user
 
 def get_current_league(
+        league_id: int = Path(..., description="ID of the league"),
         user: models.User = Depends(get_current_user),
         db: Session = Depends(get_db)
 ) -> models.League:
-    # fill later
+    league = db.query(models.League).filter(
+        models.League.user_id == user.id,
+        models.League.id == league_id).first()
+    
+    if not league:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"League with id {league_id} not found"
+        )
+    
+    return league
--- server/routers/team.py
+++ server/routers/team.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from models import models, schemas
 from database.database import get_db
-from authentication.auth import get_current_user
+from authentication.auth import get_current_league
 
 router = APIRouter(prefix="/teams", tags=["Teams"])
 
