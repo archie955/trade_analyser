4481a5ae3677e16a3dd1609a7a852adb89d1b626
archie955 <archiesmedley@gmail.com>
2026-05-08 21:13:08 +0100

    first attempt at dockerisation + alterations to frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 const App = () => {
     return (
-        <div>init</div>
+        <div>
+            <Routes>
+                <Route path="" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/*" element={<Home />} />
+            </Routes>
+        </div>
     )
 }
 
